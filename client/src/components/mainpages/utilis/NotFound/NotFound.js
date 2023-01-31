11581216f3fd68cd74942ac153fc0048ea4e158b
himaabastin <111218465+himaabastin@users.noreply.github.com>
2023-01-31 22:35:40 +0530

    reback to react

--- client/src/components/mainpages/utilis/NotFound/NotFound.js
+++ client/src/components/mainpages/utilis/NotFound/NotFound.js
@@ -1,10 +1,12 @@
 import React from 'react'
-
+import "./NotFound.css"
 const NotFound = () => {
   return (
-    <div>
-        <h1>
-        404 || Not Found
+    <div className='position-relative' style={{
+      minHeight :'calc(100vh-70px)',marginTop:"6rem"
+    }}>
+        <h1 className='position-absolute' style={{color:"#d6483e",top:"50%",left:'50%',transform:'translate(-50%,-50%)'}}>
+        404 | Not Found
 
         </h1>
     </div>
--- controllers/adminCtrl.js
+++ controllers/adminCtrl.js
@@ -83,22 +83,34 @@ const adminCtrl = {
       return res.status(500).json({ msg: err.message });
     }
   },
-  refreshToken: (req, res) => {
+  generateAccessToken: async (req, res) => {
     try {
-      const rf_token = req.cookies.refreshtoken;
-      if (!rf_token)
-        return res.status(400).json({ msg: "Please Login or Register" });
-      jwt.verify(rf_token, process.env.REFRESH_TOKEN_SECRET, (err, admin) => {
-        if (err)
-          return res.status(400).json({ msg: "Please Login or Register" });
-        const accesstoken = createAccessToken({ id: admin._id });
-        res.json({ accesstoken, rf_token });
-      });
+        const rf_token = req.cookies.refreshtoken
+        if(!rf_token) return res.status(400).json({msg: "Please login now."})
+
+        jwt.verify(rf_token, process.env.REFRESH_TOKEN_SECRET, async(err, result) => {
+            if(err) return res.status(400).json({msg: "Please login now."})
+
+            const admin = await Admins.findById(result.id).select("-password")
+          
+
+            if(!admin) return res.status(400).json({msg: "This admin does not exist."})
+
+            const access_token = createAccessToken({id: result.id})
+
+            res.json({
+                access_token,
+                admin
+            })
+        })
+        
     } catch (err) {
-      return res.status(500).json({ msg: err.message });
+        return res.status(500).json({msg: err.message})
     }
-  },
-  getAdmin: async (req, res) => {
+}
+
+,
+  getAdmin: async(req, res) => {
     try {
       const admin = await Admins.findById(req.admin.id).select("-password");
       if (!admin) return res.status(400).json({ msg: "Admin does not exist" });
@@ -108,7 +120,7 @@ const adminCtrl = {
       return res.status(500).json({ msg: err.message });
     }
   },
-};
+}
 
 const createAccessToken = (admin) => {
   return jwt.sign(admin, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1d" });
