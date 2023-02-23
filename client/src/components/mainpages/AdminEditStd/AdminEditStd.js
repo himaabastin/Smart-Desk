be4fe43a2ba28c0f8767c56beee6fa9f9a62bcac
himaabastin <111218465+himaabastin@users.noreply.github.com>
2023-02-23 15:40:23 +0530

    cloudinary backend for e-commerce set

--- client/src/components/mainpages/AdminEditStd/AdminEditStd.js
+++ client/src/components/mainpages/AdminEditStd/AdminEditStd.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useContext } from "react";
 import axios from "axios";
 import { GlobalState } from "../../../GlobalState";
 import Swal from "sweetalert2";
-import {useNavigate,useParams} from 'react-router-dom'
+import { useNavigate, useParams } from "react-router-dom";
 import "./AdminEditStd.css";
 
 const initialState = {
@@ -14,111 +14,169 @@ const initialState = {
   bloodGroup: "",
   grade: "",
   address: "",
- avatar:""
+  avatar: "",
 };
 
 function AdminEditStd() {
-//   const state=useContext(GlobalState)
-//   // console.log("globalstate",state);
-// const [isLogged,setIsLogged]=state.adminAPI.isLogged
-// const [isAdmin,setIsAdmin]=state.adminAPI.isAdmin
+  //   const state=useContext(GlobalState)
+  //   // console.log("globalstate",state);
+  // const [isLogged,setIsLogged]=state.adminAPI.isLogged
+  // const [isAdmin,setIsAdmin]=state.adminAPI.isAdmin
   const [student, setStudent] = useState(initialState);
-const navigate=useNavigate()
-const params=useParams()
+  const [imageUrl, setImageUrl] = useState("");
+  const navigate = useNavigate();
+  const params = useParams();
 
-let someFunc=async()=>{
-  if(params.id){
-        let students= await axios.get("/admin/allStudentDetails")
-        students.data.allStudentDetails.map(std =>{
-          if(std.stdAdNo === params.id ){
-          setStudent(std)}
-        })
-      }else{
-        setStudent(initialState)
-      }
-}
+  let someFunc = async () => {
+    if (params.id) {
+      let students = await axios.get("/admin/allStudentDetails");
+      students.data.allStudentDetails.map((std) => {
+        if (std.stdAdNo === params.id) {
+          setStudent(std);
+          setImageUrl(std.avatar);
+        }
+      });
+    } else {
+      setStudent(initialState);
+    }
+  };
 
-useEffect(()=>{
- someFunc()
-},[])
+  useEffect(() => {
+    someFunc();
+  }, []);
 
   const handleChangeInput = (e) => {
     const { name, value } = e.target;
     setStudent({ ...student, [name]: value });
   };
-  const imageUpload=(e)=>{
+  const imageUpload = (e) => {
     console.log(e.target.files[0]);
-    setStudent({ ...student,avatar: e.target.files[0] });
+    setStudent({ ...student, avatar: e.target.files[0] });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("==", student.avatar, "===", student.avatar.name);
+    const formdata = new FormData();
+    formdata.append("avatar", student.avatar, student.avatar.name);
+    formdata.append("name", student.name);
+    formdata.append("email", student.email);
+    formdata.append("mobile", student.mobile);
+    formdata.append("dob", student.dob);
+    formdata.append("bloodGroup", student.bloodGroup);
+    formdata.append("grade", student.grade);
+    formdata.append("address", student.address);
 
-  }
-  const handleSubmit=async(e)=>{
-    e.preventDefault()
-    console.log('==',student.avatar,"===",student.avatar.name);
-    const formdata=new FormData()
-    formdata.append('avatar',student.avatar,student.avatar.name)
-    formdata.append('name',student.name)
-    formdata.append('email',student.email)
-    formdata.append('mobile',student.mobile)
-    formdata.append('dob',student.dob)
-    formdata.append('bloodGroup',student.bloodGroup)
-    formdata.append('grade',student.grade)
-    formdata.append('address',student.address)
-
-console.log(formdata);
+    console.log(formdata);
     try {
       // if(!isAdmin && !isLogged) return Swal.fire({text:"You are not allowed to edit"})
 
-      await axios.put(`/admin/adminStdUpdate/${student.stdAdNo}`,formdata)
-      navigate("/studentManagement")
+      await axios.put(`/admin/adminStdUpdate/${student.stdAdNo}`, formdata);
+      navigate("/studentManagement");
     } catch (err) {
       Swal.fire({
-        text:err.response.data.msg
-      })
+        text: err.response.data.msg,
+      });
     }
-  }
+  };
   return (
     <div className="edit_std">
-     
-      <form className="form-style-9" onSubmit={handleSubmit}>
-      <h3 style={{color:" #556354"}}>EDIT STUDENT</h3>
-     <ul>
-       <li>
-
-      
-          <label htmlFor="name">Name</label>
-          <input type="text" name="name" id="name" value={student.name} onChange={handleChangeInput} />
-          </li>
-        <li>
-          <label htmlFor="email">Email</label>
-          <input type="text" name="email" id="email"value={student.email}  onChange={handleChangeInput} />
+      <form className="form-style-9" onSubmit={handleSubmit} encType="multipart/form-data">
+        <h3 style={{ color: " #556354" }}>EDIT STUDENT</h3>
+        <ul>
+          <li>
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              value={student.name}
+              onChange={handleChangeInput}
+            />
           </li>
           <li>
-          <label htmlFor="mobile">Mobile</label>
-          <input type="text" name="mobile" id="mobile"className="field-style field-split align-left" value={student.mobile}  onChange={handleChangeInput}/>
+            <label htmlFor="email">Email</label>
+            <input
+              type="text"
+              name="email"
+              id="email"
+              value={student.email}
+              onChange={handleChangeInput}
+            />
           </li>
           <li>
-          <label htmlFor="dob">Date of Birth</label>
-          <input type="text" name="dob" id="dob"className="field-style field-split align-left"  value={student.dob} onChange={handleChangeInput}/>
+            <label htmlFor="mobile">Mobile</label>
+            <input
+              type="text"
+              name="mobile"
+              id="mobile"
+              className="field-style field-split align-left"
+              value={student.mobile}
+              onChange={handleChangeInput}
+            />
           </li>
           <li>
-          <label htmlFor="bloodGroup">Blood Group</label>
-          <input type="text" name="bloodGroup" id="bloodGroup" className="field-style field-split align-left" value={student.bloodGroup}  onChange={handleChangeInput}/>
+            <label htmlFor="dob">Date of Birth</label>
+            <input
+              type="text"
+              name="dob"
+              id="dob"
+              className="field-style field-split align-left"
+              value={student.dob}
+              onChange={handleChangeInput}
+            />
           </li>
           <li>
-          <label htmlFor="grade">Class</label>
-          <input type="text" name="grade" id="grade" className="field-style field-split align-left" value={student.grade}  onChange={handleChangeInput}/>
+            <label htmlFor="bloodGroup">Blood Group</label>
+            <input
+              type="text"
+              name="bloodGroup"
+              id="bloodGroup"
+              className="field-style field-split align-left"
+              value={student.bloodGroup}
+              onChange={handleChangeInput}
+            />
           </li>
           <li>
-          <label htmlFor="address">Address</label>
-          <input type="text" name="address" id="address" className="field-style field-split align-left" value={student.address}  onChange={handleChangeInput}/>
+            <label htmlFor="grade">Class</label>
+            <input
+              type="text"
+              name="grade"
+              id="grade"
+              className="field-style field-split align-left"
+              value={student.grade}
+              onChange={handleChangeInput}
+            />
           </li>
           <li>
-          <label htmlFor="avatar">Upload image</label>
-          <input type="file" name="avatar" id="avatar" className="field-style"   onChange={imageUpload}/>
+            <label htmlFor="address">Address</label>
+            <input
+              type="text"
+              name="address"
+              id="address"
+              className="field-style field-split align-left"
+              value={student.address}
+              onChange={handleChangeInput}
+            />
           </li>
           <li>
-        <button className="btnStd " type="submit">Edit</button>
-        </li>
+            <label htmlFor="avatar">Upload image</label>
+            <img className="imgedit"
+              src={`http://localhost:3000/public/images/${student.avatar}`}
+            />
+            <input
+              type="file"
+              name="avatar"
+              id="avatar"
+              className="field-style"
+              onChange={imageUpload}
+            
+            />
+          </li>
+          <li>
+            <button className="btnStd " type="submit">
+              Edit
+            </button>
+          </li>
         </ul>
       </form>
     </div>
--- routes/upload.js
+++ routes/upload.js
@@ -1,33 +1,65 @@
 const router=require('express').Router()
-const cloudinary=require('cloudinary')
+const {cloudinary}=require("../utilis/cloudinary")
+const {CloudinaryStorage} =require("multer-storage-cloudinary")
 const auth=require("../middleware/auth")
 const authAdmin=require("../middleware/authAdmin")
+const fs=require('fs')
+const multer=require('multer');
 
+//multer
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    folder: 'my-folder-name',
+    allowedFormats: ['jpg', 'png'],
+    // transformation: [{ width: 500, height: 500, crop: 'limit' }],
+  });
 
-cloudinary.config({
-    cloud_name:process.env.CLOUD_NAME,
-    api_key:process.env.CLOUD_API_KEY,
-    api_secret:process.env.CLOUD_API_SECRET
-})
-
-
+  const upload=multer({storage:storage})
 //upload image
 
-router.post("/upload",(req,res)=>{
+router.post("/upload",upload.single("file"),authAdmin,(req,res)=>{
     try {
-        console.log(req.files);
-        // if(!req.files || Object.keys(req.files).length === 0)
-        // return res.status(400).json({msg:'No files were uploaded'})
+        console.log(req.file);
+        if(!req.files || Object.keys(req.files).length === 0)
+        return res.status(400).json({msg:'No files were uploaded'})
 
-        // const file=req.files.file
-        //     if(file.size > 1024*1024) return res.status(400).json({msg:"File size large"})
-        //     if(file.mimetype !== 'image/jpeg' && file.mimetype !== 'image/png')
-        //     return res.status(400).json({msg:"File format is inc"})
-        res.json('test upload')
+        const file=req.files.file
+            if(file.size > 1024*1024) {
+                removeTmp(file.tempFilePath)
+                return res.status(400).json({msg:"File size large"})}
+            if(file.mimetype !== 'image/jpeg' && file.mimetype !== 'image/png'){
+                removeTmp(file.tempFilePath)
+
+            return res.status(400).json({msg:"File format is incorrect"})}
+
+            cloudinary.uploader.upload(file.tempFilePath,{folder:"test"},async(err,result)=>{
+                if(err) throw err
+                removeTmp(file.tempFilePath)
+
+                res.json({public_id:result.public_id,url:result.secure_url})
+            })
+        // res.json('test upload')
     } catch (err) {
         res.status(500).json({msg:err.message})
     }
 })
 
+router.post('/destroy',authAdmin,(req,res)=>{
+    try {
+const{public_id}=req.body
+      if(!public_id) return res.status(400).json({msg:"No images Selected"})
+      cloudinary.uploader.destroy(public_id,async(err,result)=>{
+        if(err) throw err
+        res.json({msg:"Deleted Image"})
+      })
+    } catch (err) {
+       return res.status(500).json({msg:err.message})
+    }
+})
 
+const removeTmp=(path)=>{
+fs.unlink(path,err=>{
+    if(err) throw err
+})
+}
 module.exports=router
