040501eac3839d5415c7be8a91e21148e152d295
himaabastin <111218465+himaabastin@users.noreply.github.com>
2023-02-26 16:23:24 +0530

    get timetable(tchr) frontend set

--- client/src/components/mainpages/TimeTableTchr/TimeTableTchr.js
+++ client/src/components/mainpages/TimeTableTchr/TimeTableTchr.js
@@ -1,84 +1,54 @@
-import React,{useEffect, useState} from 'react'
-import "./TimeTableTchr.css"
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import "./TimeTableTchr.css";
+import axios from "axios";
+const TimeTableTchr = () => {
+  const [timeTable, setTimeTable] = useState([]);
+  const fetchTimeTable = async () => {
+    const teacher = await axios.get("teacher/refresh_token");
+    const tTable = teacher.data.timeTable;
+    console.log(tTable);
+    setTimeTable(tTable);
+  };
 
-function TimeTableTchr() {
-  // Define the days of the week and time 
-  const [timeTable,setTimeTable]=useState([])
-  const [day,setDay]=useState([])
-  const  fetchTimeTable=async(e)=>{
-    try {
-      let table=await axios.get("/teacher/refresh_token")
-      let fetchdays=await axios.get("/teacher/getDay")
-     setDay(fetchdays.data);
-      console.log("hi",table.data.timeTable);
-      setTimeTable(table.data.timeTable)
-    } catch (err) {
-      
-    }
-  }
-  useEffect(()=>{
-    fetchTimeTable()
-  },[])
-  
-  const days = [day]
-  const times = [
-    '9:00 AM - 10:00 AM',
-    '10:00 AM - 11:00 AM',
-    '11:00 AM - 12:00 PM',
-    '12:00 PM - 1:00 PM',
-    '1:00 PM - 2:00 PM',
-    '2:00 PM - 3:00 PM',
-    '3:00 PM - 4:00 PM',
-  ];
-
-  // Create a state variable to hold the table data
-  const [tableData, setTableData] = useState(() => {
-    // Initialize an empty 2D array with the days as rows and times as columns
-    const initialTableData = Array.from(Array(days.length), () =>
-      Array.from(Array(times.length), () => '')
-    );
-    // Add the days of the week and time slots to the table data
-    for (let i = 0; i < days.length; i++) {
-      initialTableData[i].unshift(days[i]);
-    }
-    initialTableData.unshift(['', ...times]);
-    return initialTableData;
+  useEffect(() => {
+    fetchTimeTable();
+  }, []);
+  const sortedTable = timeTable.sort((a, b) => {
+    const daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+    return daysOfWeek.indexOf(a.day) - daysOfWeek.indexOf(b.day);
   });
-
-  // Handle changes to the table data
-  // const handleTableChange = (event, row, col) => {
-  //   const newData = [...tableData];
-  //   if (col > 0) {
-  //     newData[row][col] = event.target.value;
-  //   }
-  //   setTableData(newData);
-  // };
-
-  // Render the table
   return (
-    <div className="timetable">
+    <div className="timetable-container">
+<button className="update-button">Update</button>
       <table>
         <thead>
-          {tableData[0].map((cell, colIndex) => (
-            <th key={colIndex}>{cell}</th>
-          ))}
+          <tr>
+            <th>Day</th>
+            <th> 1</th>
+            <th> 2</th>
+            <th>3</th>
+            <th> 4</th>
+            <th> 5</th>
+            <th>6</th>
+          </tr>
         </thead>
         <tbody>
-          {tableData.slice(1).map((row, rowIndex) => (
-            <tr key={rowIndex}>
-              {row.map((cell, colIndex) => (
-                <td key={`${rowIndex}-${colIndex}`}>
-                   {colIndex === 0 ? days[rowIndex] :"subjects"
-          }
-                </td>
-              ))}
+          {/* {timeTable.map((day)=>)} */}
+          {sortedTable.map((day) => (
+            <tr key={day._id}>
+              <td>{day.day}</td>
+              <td>{day.subjects[0]}</td>
+              <td>{day.subjects[1]}</td>
+              <td>{day.subjects[2]}</td>
+              <td>{day.subjects[3]}</td>
+              <td>{day.subjects[4]}</td>
+              <td>{day.subjects[5]}</td>
             </tr>
           ))}
         </tbody>
       </table>
     </div>
   );
-}
+};
 
-export default TimeTableTchr
+export default TimeTableTchr;
